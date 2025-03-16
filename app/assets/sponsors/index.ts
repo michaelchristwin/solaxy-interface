b24f051c36fe090c6697fd7b84e710a27dec1cb0
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-03-16 20:09:14 +0100

    chore: Added sponsors grid

--- app/assets/sponsors/index.ts
+++ app/assets/sponsors/index.ts
@@ -1,6 +1,6 @@
 import Four626 from "./4626.svg";
 import BondingCurve from "./bonding-curve.jpg";
-import TEC from "./tec.svg";
+import TEC from "./tec.png";
 import Solady from "./solady.svg";
 import Four626_2 from "./4626-2.svg";
 
